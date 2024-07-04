720b16a1b75b2e68a9c7630d77766c0264a0c62a
ashwin mali <118095859+ashwinmali2004@users.noreply.github.com>
2024-07-04 20:48:14 +0530

    commited back

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     // console.log("displaying albums")
-    let a = await fetch(`./songs/`)
+    let a = await fetch(`http://127.0.0.1:5500/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
